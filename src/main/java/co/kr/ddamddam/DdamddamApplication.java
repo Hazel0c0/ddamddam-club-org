6d7f24627deb79b2f3c88596b102ccdc704a0e6e
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-07 17:22:06 +0900

    first commit

---
+++ src/main/java/co/kr/ddamddam/DdamddamApplication.java
@@ -0,0 +1,13 @@
+package co.kr.ddamddam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdamddamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdamddamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/ddamddam/DdamddamApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.ddamddam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdamddamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
