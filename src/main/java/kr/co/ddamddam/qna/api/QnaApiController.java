b1dded72b1413c161599efcb0468433c73a57df2
JOY0987 <jjoyewon@gmail.com>
2023-06-08 16:37:06 +0900

    Feat: Qna 게시글 전체조회
    - Postman 테스트까지 완료

--- src/main/java/kr/co/ddamddam/qna/api/QnaApiController.java
+++ src/main/java/kr/co/ddamddam/qna/api/QnaApiController.java
@@ -1,14 +1,33 @@
 package kr.co.ddamddam.qna.api;
 
+import kr.co.ddamddam.common.response.ApplicationResponse;
+import kr.co.ddamddam.qna.dto.page.PageDTO;
+import kr.co.ddamddam.qna.dto.response.QnaListResponseDTO;
 import kr.co.ddamddam.qna.service.QnaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@RequestMapping("/api/ddamddam/qna")
 public class QnaApiController {
 
     private final QnaService qnaService;
+
+    // QNA 페이지 전체 목록 조회
+    @GetMapping
+    public ApplicationResponse<QnaListResponseDTO> getList(PageDTO pageDTO) {
+
+        log.info("/api/qna/page/{}/size/{}", pageDTO.getPage(), pageDTO.getSize());
+
+        QnaListResponseDTO list = qnaService.getList(pageDTO);
+
+        return ApplicationResponse.ok(list);
+    }
 }
