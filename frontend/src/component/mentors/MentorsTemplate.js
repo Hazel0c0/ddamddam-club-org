364846b1b95d9791e3ee51248ee7eec40747a58a
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-09 11:51:09 +0900

    Design:멘토 페이지

---
+++ frontend/src/component/mentors/MentorsTemplate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MentorsMain from "./MentorsMain";
+import MentorsList from "./MentorsList";
+
+const MentorsTemplate = () => {
+    return (
+        <>
+            <MentorsMain/>
+            <MentorsList/>
+        </>
+    );
+};
+
+export default MentorsTemplate;
---
+++ frontend/src/component/projects/ProjectsMain.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectsMain = () => {
+    return (
+        <div>
+            테스트
+        </div>
+    );
+};
+
+export default ProjectsMain;
