7b2418d2d6aaa1ce714a42b07018581117c0c7d1
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-10 17:52:20 +0900

    Feat:멘토 페이지 전체 리스트 출력 완료 (Profile-img)만 수정하면 됨

--- frontend/src/component/mentors/MentorsTemplate.js
+++ frontend/src/component/mentors/MentorsTemplate.js
@@ -4,28 +4,10 @@ import MentorsList from "./MentorsList";
 import {MENTOR} from "../common/config/HostConfig";
 
 const MentorsTemplate = () => {
-    const [mentorsList, setMentorsList] = useState([]);
-
-    useEffect( () =>{
-        fetch(MENTOR)
-            .then(res =>{
-                if (res.status === 500){
-                    alert('잠시 후 다시 접속해주세요.[서버오류]');
-                    return;
-                }
-                return res.json();
-            })
-            .then(result =>{
-                if (!!result){
-                    setMentorsList(result);
-                }
-            });
-        });
-
     return (
         <>
             <MentorsMain/>
-            <MentorsList mentorList = {mentors}/>
+            <MentorsList/>
         </>
     );
 };
