0c6a0daab7c16e79a2bafe66203be8a147d540f6
JOY0987 <jjoyewon@gmail.com>
2023-06-11 17:09:03 +0900

    Feat: QNA 댓글 조회 구현
    - 댓글 더미데이터 생성문 test에 추가

--- src/main/java/kr/co/ddamddam/qna/qnaReply/repository/QnaReplyRepository.java
+++ src/main/java/kr/co/ddamddam/qna/qnaReply/repository/QnaReplyRepository.java
@@ -3,5 +3,9 @@ package kr.co.ddamddam.qna.qnaReply.repository;
 import kr.co.ddamddam.qna.qnaReply.entity.QnaReply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QnaReplyRepository extends JpaRepository<QnaReply, Long> {
+    List<QnaReply> findByQnaQnaIdxOrderByQnaReplyDateAsc(Long boardIdx);
+
 }
