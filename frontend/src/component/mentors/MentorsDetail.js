bf1fc18313caa150fdf05baa8effaad0ed0873d7
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-11 17:31:51 +0900

    Feat:멘토 페이지 상세보기 값 받아오기 완료

---
+++ frontend/src/component/mentors/MentorsDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Common from "../common/Common";
+
+const MentorsDetail = () => {
+    return (
+        <Common>
+
+        </Common>
+    );
+};
+
+export default MentorsDetail;
