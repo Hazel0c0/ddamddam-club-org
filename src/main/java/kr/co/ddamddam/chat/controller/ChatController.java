361ed1876180b5da11e707486febda4cb39d32ad
oslob99 <jkh991116@naver.com>
2023-06-11 17:30:10 +0900

    Feat: 채팅 back 기능 구현 완료

--- src/main/java/kr/co/ddamddam/chat/controller/ChatController.java
+++ src/main/java/kr/co/ddamddam/chat/controller/ChatController.java
@@ -41,7 +41,7 @@ public class ChatController {
 
     // 멘토의 채팅방 전체 조회
     // 멘토인지 검증은 프론트 단에서 검증하기(수정,삭제처럼)
-    @GetMapping("/list/{mentorIdx}")
+    @GetMapping("/mentor/list/{mentorIdx}")
     public ResponseEntity<?> list(
             @PathVariable Long mentorIdx
     ){
@@ -51,7 +51,27 @@ public class ChatController {
     }
 
     // 멘티의 채팅방 개별 조회 (채팅 내용으로 바로 뿌려주기)
+    @GetMapping("/mentee/list/{mentorIdx}")
+    public ResponseEntity<?> detail(
+            @PathVariable Long mentorIdx
+    ){
+        Long senderIdx = 2L;
 
+        List<ChatMessageResponseDTO> list = chatService.getDetail(mentorIdx,senderIdx);
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    // 채팅방 삭제
+    @DeleteMapping("/{roomId}")
+    public ResponseEntity<?> delete(
+            @PathVariable Long roomId
+    ){
+
+        chatService.delete(roomId);
+
+        return ResponseEntity.ok().body("삭제 성공!");
+    }
 
 
     // 기타 API 엔드포인트 (채팅방 목록 조회, 채팅 메시지 조회 등) 추가
