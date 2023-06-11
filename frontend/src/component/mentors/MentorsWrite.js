15441b877d362b6b86516c74bd7c661da87aa8bb
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-11 18:22:26 +0900

    Feat:상세보기 완료 후 글쓰기 폼

--- frontend/src/component/mentors/MentorsWrite.js
+++ frontend/src/component/mentors/MentorsWrite.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Common from "../common/Common";
 
 const MentorsWrite = () => {
     return (
-        <div>
-            글쓰기 폼 영역
-        </div>
+        <Common className={'mentors-top-wrapper'}>
+            <div className={'title-wrapper'}>
+                <p className={'main-title'}>Mentoring</p>
+                <p className={'sub-title'}>멘토링을 통해 성장하는 동료들과 함께 협업하고 새로운 아이디어를 발전시켜보세요.</p>
+            </div>
+
+            <section className={'write-form-wrapper'}>
+                <div className={'title-input-wrapper'}>
+                    <h1>제목</h1>
+                    <input type={"text"} placeholder={'제목을 입력하세'}/>
+                </div>
+                <div className={'select-input-wrapper'}>
+
+                </div>
+            </section>
+        </Common>
     );
 };
 
