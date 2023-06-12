4d058c0944b1f373a181248592db9aca26f5a238
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-13 00:02:58 +0900

    FEAT:qnaList불러오기 완료

--- frontend/src/component/qna/QnaTemplate.js
+++ frontend/src/component/qna/QnaTemplate.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import QnaMain from "./QnaMain";
 import QnaSearch from "./QnaSearch";
+import QnaList from "./QnaList";
 
 const QnaTemplate = () => {
     return (
         <>
             <QnaMain />
             <QnaSearch />
+            <QnaList />
         </>
     );
 };
