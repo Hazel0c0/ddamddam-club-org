3f620db52afab9b3c4e46e4b1266fbc7d9d1d65b
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-06-15 15:19:35 +0900

    Feat: 회원가입 백엔드 구현

---
+++ src/main/java/kr/co/ddamddam/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package kr.co.ddamddam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//전역 크로스 오리진 설정
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry
+                .addMapping("/api/**") // 어떤 요청에 대해서 허용할지?
+                .allowedOrigins("http://localhost:3000") // 어떤 클라이언트를 허용할지
+                .allowedMethods("*") // 어떤 요청방식을 허용할지
+                .allowedHeaders("*") // 어떤 요청 헤더를 허용할지
+                .allowCredentials(true) // 쿠키 전달을 허용할것인지
+                .maxAge(3600) // 캐싱 시간을 설정
+        ;
+    }
+
+}
