8f28d06cd823d660de1de2933b8c43232895b0e1
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-18 14:17:23 +0900

    feat : review List 완료 css 나중에

--- src/main/java/kr/co/ddamddam/config/websocket/WebSocketConfig.java
+++ src/main/java/kr/co/ddamddam/config/websocket/WebSocketConfig.java
@@ -1,14 +1,15 @@
 package kr.co.ddamddam.config.websocket;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-@Component
+@Configuration
 public class WebSocketConfig {
 
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
-    }
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter(){
+//        return new ServerEndpointExporter();
+//    }
 }
