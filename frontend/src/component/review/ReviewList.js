cebcf44e5aacd18ae009b4df14ded90444adb75c
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-18 14:17:23 +0900

    feat : review List 완료 css 나중에

--- frontend/src/component/review/ReviewList.js
+++ frontend/src/component/review/ReviewList.js
@@ -4,11 +4,11 @@ import viewIcon from "../../src_assets/view-icon.png";
 import speechBubble from "../../src_assets/speech-bubble.png";
 import './scss/ReviewList.scss'
 import {IoIosArrowForward} from 'react-icons/io';
-import {QNA} from "../common/config/HostConfig";
+import {QNA, REVIEW} from "../common/config/HostConfig";
 import {Link} from "react-router-dom";
 
 const QnaList = ({searchValue}) => {
-    const [qnaList, setQnaList] = useState([]);
+    const [reviewList, setReviewList] = useState([]);
     const [pageNation, setPageNation] = useState([]);
 
     //상세보기 이동
@@ -16,17 +16,7 @@ const QnaList = ({searchValue}) => {
 
     //전체 목록 렌더링 필터 async
     const asyncReviewList = async () => {
-        let responseUrl;
-        if (searchValue === '' || searchValue === '전체') {
-            responseUrl = '?page=4&size=10'
-        } else if (searchValue === '미채택') {
-            responseUrl = '/non-adopts';
-        } else if (searchValue === '채택완료') {
-            responseUrl = '/adopts';
-        }
-        console.log(`searchValue = ${searchValue}`)
-        console.log(`responseUrl = ${responseUrl}`)
-        const res = await fetch(`${QNA}${responseUrl}`, {
+        const res = await fetch(`${REVIEW}/list`, {
             method: 'GET',
             headers: {'content-type': 'application/json'}
         });
@@ -36,57 +26,47 @@ const QnaList = ({searchValue}) => {
             return;
         }
 
-        const qnaList = await res.json();
-        console.log(qnaList)
-        console.log(`qnaList = ${qnaList.payload}`);
-        setQnaList(qnaList.payload.qnas);
-        setPageNation(qnaList.payload.pageInfo);
+        const reivewList = await res.json();
+        console.log(reivewList)
+        setReviewList(reivewList.responseList);
+        setPageNation(reivewList.pageInfo);
 
     }
 
     // 전체 목록 리스트 출력
     useEffect(() => {
-        asyncQnaList();
-    }, [qnaDetailBoardIdx, searchValue]);
-
+        asyncReviewList();
+    }, []);
 
 
     return (
         <Common className={'qna-list-wrapper'}>
-            {qnaList.map((qna) => (
-                <section className={'qna-list'} key={qna.boardIdx}>
-                    {qna.boardAdoption === 'Y'
-                        ? <div className={'checked'} key={qna.boardAdoption}>
-                            채택완료</div>
-                        : <div className={'not-checked'} key={qna.boardAdoption}>
-                            미채택</div>
-                    }
+            {reviewList.map((review) => (
+                <section className={'qna-list'} key={review.reviewIdx}>
 
                     <section className={'text-wrapper'}>
-                        <div className={'text-title'} key={qna.boardTitle}>{qna.boardTitle}</div>
-                        <div className={'text-content'} key={qna.boardContent}>{qna.boardContent}</div>
-                        <ul className={'text-hashTag'}>
-                            {qna.hashtagList.map((hashTag, index) => (
-                                <li key={index}>#{hashTag}</li>
-                            ))}
-                        </ul>
+                        <div className={'title-wrapper'}>
+                            <div className={'text-title'} key={review.reviewTitle}>{review.reviewTitle}</div>
+                            <div className={'text-reviewJob'} key={review.reviewJob}><span>직무</span> {review.reviewJob}
+                            </div>
+                            <div className={'text-title'} key={review.reviewTitle}><span>근속년수</span>{review.reviewTitle}
+                            </div>
+                            <div className={'text-title'} key={review.reviewLocation}>
+                                <span>위치</span>{review.reviewLocation}</div>
+                        </div>
+                        <div className={'text-content'} key={review.reviewContent}>{review.reviewContent}</div>
                     </section>
                     <section className={'qna-info'}>
-                        <div className={'qna-writer'} key={qna.boardWriter}>{qna.boardWriter}</div>
                         <div className={'icon-wrapper'}>
                             <div className={'view-count-wrapper'}>
-                                <img src={viewIcon} alt={'view-count'} className={'view-count-icon'}/><span>{qna.viewCount}</span>
-                            </div>
-                            <div className={'speech-count-wrapper'}>
-                                <img src={speechBubble} alt={'view-count'}
-                                     className={'speech-count-icon'}/><span>{qna.replyCount}</span>
+                                <img src={viewIcon} alt={'view-count'} className={'view-count-icon'}/>
+                                <span>{review.reviewView}</span>
                             </div>
                         </div>
-                        <div className={'write-date'} key={qna.boardDate}>{qna.boardDate}</div>
+                        <div className={'write-date'} key={review.boardDate}>{review.boardDate}</div>
                     </section>
 
-                    <Link to={`/api/ddamddam/qna/${qna.boardIdx}`}>
-                        {/*<div className={'go-detail'} onClick={() => detailHandler(qna.boardIdx)}>*/}
+                    <Link to={`/api/ddamddam/reviews/dertail/${review.reviewIdx}`}>
                         <div className={'go-detail'}>
                             <div className={'go-detail-text'}>더보기</div>
                             <i className={'go-detail-icon'}><IoIosArrowForward/></i>
