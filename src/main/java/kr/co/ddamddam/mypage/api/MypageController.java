d97e97e3ad83fc2ef29495c9b414fcc1511373d0
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-06-20 16:06:46 +0900

    feat back/ mypage - project 목록 조회

--- src/main/java/kr/co/ddamddam/mypage/api/MypageController.java
+++ src/main/java/kr/co/ddamddam/mypage/api/MypageController.java
@@ -1,17 +1,22 @@
 package kr.co.ddamddam.mypage.api;
 
+import io.swagger.v3.oas.annotations.Parameter;
 import kr.co.ddamddam.config.security.TokenUserInfo;
 import kr.co.ddamddam.mypage.dto.page.PageDTO;
 import kr.co.ddamddam.mypage.dto.response.MypageBoardPageResponseDTO;
 import kr.co.ddamddam.mypage.dto.response.MypageBoardResponseDTO;
+import kr.co.ddamddam.mypage.dto.response.MypageProjectResponseDTO;
 import kr.co.ddamddam.mypage.service.MypageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -32,5 +37,15 @@ public class MypageController {
 
         return ResponseEntity.ok().body(boardList);
     }
+    @GetMapping("/project-list/{userIdx}")
+    public ResponseEntity<?> getProjectList(
+//        @AuthenticationPrincipal TokenUserInfo tokenUserInfo
+        @PathVariable Long userIdx
 
+    ){
+//        Long userIdx = Long.valueOf(tokenUserInfo.getUserIdx());
+        List<MypageProjectResponseDTO> myProjectList = myPageService.getProjectList(userIdx);
+
+        return ResponseEntity.ok().body(myProjectList);
+    }
 }
--- src/main/java/kr/co/ddamddam/project/entity/applicant/ApplicantOfFront.java
+++ src/main/java/kr/co/ddamddam/project/entity/applicant/ApplicantOfFront.java
@@ -22,6 +22,4 @@ public class ApplicantOfFront {
 
   @Id
   private Long userIdx;
-
-
 }
