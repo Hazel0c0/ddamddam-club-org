7b4250559f1ea47c50ddc54b8c82ca5f9daddb93
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-06-20 20:27:06 +0900

    FEAT : 검색 완료

--- frontend/src/component/qna/QnaList.js
+++ frontend/src/component/qna/QnaList.js
@@ -8,120 +8,76 @@ import {QNA} from "../common/config/HostConfig";
 import {Link, useNavigate} from "react-router-dom";
 import PageNation from "../common/pageNation/PageNation";
 import {getToken} from "../common/util/login-util";
+import QnaTotal from "./QnaTotal";
+import QnaNoAdoption from "./QnaNoAdoption";
+import QnaAdoption from "./QnaAdoption";
+import QnaSearchKeyword from "./QnaSearchKeyword";
 
-const QnaList = ({searchValue}) => {
-    const [qnaList, setQnaList] = useState([]);
-    const [pageNation, setPageNation] = useState([]);
-    const [clickCurrentPage, setClickCurrentPage] = useState(1);
-    const [test, setTest] = useState(true);
-    //상세보기 이동
-    const [qnaDetailBoardIdx, setqnaDetailBoardIdx] = useState([]);
+const QnaList = ({searchValue, searchKeyword}) => {
+    const [pageTrue, setPageTrue] = useState({
+        total: true,
+        notAdoption: false,
+        Adoption: false,
+        search: false
+    })
 
-    //로그인 검증
-    const ACCESS_TOKEN = getToken();
-    const redirection = useNavigate();
-    const loginCheckHandler = (e) =>{
-        console.log(`ACCESS_TOKEN = ${ACCESS_TOKEN}`)
-        if (ACCESS_TOKEN === '' || ACCESS_TOKEN === null){
-            alert('로그인 후 이용가능합니다.')
-            e.preventDefault();
-            redirection('/login');
-            // return;
-        }
-    }
-
-    const asyncQnaList = async () => {
-        let responseUrl;
-        if (searchValue === '' || searchValue === '전체') {
-
-            responseUrl = `?page=${clickCurrentPage}&size=10`
-        } else if (searchValue === '미채택') {
-            responseUrl = `/non-adopts?page=${clickCurrentPage}&size=10`;
-        } else if (searchValue === '채택완료') {
-            responseUrl = `/adopts?page=${clickCurrentPage}&size=10`;
-        }
-        console.log(`searchValue = ${searchValue}`)
-        console.log(`responseUrl = ${responseUrl}`)
-        const res = await fetch(`${QNA}${responseUrl}`, {
-            method: 'GET',
-            headers: {'content-type': 'application/json'}
-        });
-
-        if (res.status === 500) {
-            alert('잠시 후 다시 접속해주세요.[서버오류]');
-            return;
-        }
-
-        const qnaList = await res.json();
-        console.log(qnaList)
-        console.log(`qnaList = ${qnaList.payload}`);
-        setQnaList(qnaList.payload.qnas);
-        setPageNation(qnaList.payload.pageInfo);
-
-    }
+    const [loginCheck, setLoginCheck] = useState(false);
 
     // 전체 목록 리스트 출력
     useEffect(() => {
-        asyncQnaList();
-    }, [qnaDetailBoardIdx, searchValue, clickCurrentPage]);
+        //로그인 검증
+        const ACCESS_TOKEN = getToken();
+        if (ACCESS_TOKEN !== '' || ACCESS_TOKEN !== null) {
+            setLoginCheck(true);
 
-    //현재 페이지 설정
-    const currentPageHandler = (clickPageNum) =>{
-        console.log(`페이지 클릭 시 현재 페이지 번호 : ${clickPageNum}`)
-        setClickCurrentPage(clickPageNum);
-    }
+        }
+        console.log(`searchValue = ${searchKeyword}`)
+        if (searchKeyword !== null && searchKeyword !== '') {
+            setPageTrue({
+                total: false,
+                notAdoption: false,
+                Adoption: false,
+                search: true
+            });
+            return;
+        }
 
+        if (searchValue === '' || searchValue === '전체') {
+            searchKeyword = '';
+            setPageTrue({
+                total: true,
+                notAdoption: false,
+                Adoption: false,
+                search: false
+            });
+        } else if (searchValue === '미채택') {
+            searchKeyword = '';
+            setPageTrue({
+                total: false,
+                notAdoption: true,
+                Adoption: false,
+                search: false
+            });
+        } else if (searchValue === '채택완료') {
+            searchKeyword = '';
+            setPageTrue({
+                total: false,
+                notAdoption: false,
+                Adoption: true,
+                search: false
+            });
+        }
+
+
+    }, [searchValue, searchKeyword]);
 
     return (
         <Common className={'qna-list-wrapper'}>
-            {qnaList.map((qna) => (
-                <section className={'qna-list'} key={qna.boardIdx}>
-                    {qna.boardAdoption === 'Y'
-                        ? <div className={'checked'} key={qna.boardAdoption}>
-                            채택완료</div>
-                        : <div className={'not-checked'} key={qna.boardAdoption}>
-                            미채택</div>
-                    }
-
-                    <section className={'text-wrapper'}>
-                        <div className={'text-title'} key={qna.boardTitle}>{qna.boardTitle}</div>
-                        <div className={'text-content'} key={qna.boardContent}>{qna.boardContent}</div>
-                        <ul className={'text-hashTag'}>
-                            {qna.hashtagList.map((hashTag, index) => (
-                                <li key={index}>#{hashTag}</li>
-                            ))}
-                        </ul>
-                    </section>
-                    <section className={'qna-info'}>
-                        <div className={'qna-writer'} key={qna.boardWriter}>{qna.boardWriter}</div>
-                        <div className={'icon-wrapper'}>
-                            <div className={'view-count-wrapper'}>
-                                <img src={viewIcon} alt={'view-count'} className={'view-count-icon'}/><span>{qna.viewCount}</span>
-                            </div>
-                            <div className={'speech-count-wrapper'}>
-                                <img src={speechBubble} alt={'view-count'}
-                                     className={'speech-count-icon'}/><span>{qna.replyCount}</span>
-                            </div>
-                        </div>
-                        <div className={'write-date'} key={qna.boardDate}>{qna.boardDate}</div>
-                    </section>
-
-                    <Link to={`/api/ddamddam/qna/${qna.boardIdx}`} onClick={loginCheckHandler}>
-                        {/*<div className={'go-detail'} onClick={() => detailHandler(qna.boardIdx)}>*/}
-                        <div className={'go-detail'}>
-                            <div className={'go-detail-text'}>더보기</div>
-                            <i className={'go-detail-icon'}><IoIosArrowForward/></i>
-                        </div>
-                    </Link>
-                </section>
-            ))}
-
-            <ul>
-                <PageNation
-                    pageNation={pageNation}
-                    currentPageHandler={currentPageHandler}
-                    clickCurrentPage={clickCurrentPage} />
-            </ul>
+            {/*{pageTrue.search ? 'pageTrue.search의 값 true' : 'pageTrue.search의 값 false'}*/}
+            {pageTrue.total && <QnaTotal loginCheck={loginCheck}/>}
+            {pageTrue.notAdoption && <QnaNoAdoption loginCheck={loginCheck}/>}
+            {pageTrue.Adoption && <QnaAdoption loginCheck={loginCheck}/>}
+            {pageTrue.search && <QnaSearchKeyword loginCheck={loginCheck} searchKeyword={searchKeyword}/>}
         </Common>
     );
 };
