390daab4bfb4ca798ca032c3ed2084863efe8943
JOY0987 <jjoyewon@gmail.com>
2023-06-21 14:03:06 +0900

    Fix: 카카오 로그인 수정
    - App.js 카카오로그인 라우터 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ import ReviewList from "./component/review/ReviewList";
 import ReviewTemplate from "./component/review/ReviewTemplate";
 import ReviewDetail from "./component/review/ReviewDetail";
 import ReviewWrite from "./component/review/ReviewWrite";
+import OAuth2RedirectHandler from "./component/snslogin/OAuth2RedirectHandler";
 
 
 function App() {
@@ -84,6 +85,9 @@ function App() {
 
                 {/*회원가입*/}
                 <Route path={'/join'} element={<UserJoin/>}></Route>
+
+                {/*로그인*/}
+                <Route path={`/oauth/callback/kakao`} element={<OAuth2RedirectHandler/>}></Route>
 
             </Routes>
             <Footer/>
