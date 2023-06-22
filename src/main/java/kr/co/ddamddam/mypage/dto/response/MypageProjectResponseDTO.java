24f1e2fc1f278fdded9022cf0941f3dcefdcd6cf
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-06-22 12:14:55 +0900

    fix project main img 불러오기

--- src/main/java/kr/co/ddamddam/mypage/dto/response/MypageProjectResponseDTO.java
+++ src/main/java/kr/co/ddamddam/mypage/dto/response/MypageProjectResponseDTO.java
@@ -21,6 +21,7 @@ public class MypageProjectResponseDTO {
           프로젝트 개최자
           프로젝트 참가자 - 백/프론트
      */
+    // TODO idx 담기
     private String boardTitle;
     private String boardWriter;
     private UserPosition writerPosition;
