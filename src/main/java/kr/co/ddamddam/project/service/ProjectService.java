5bdeda37c631c993c46ef84adbb733453dc2a6f9
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-06-22 11:48:27 +0900

    fix project modify 작성일자 수정

--- src/main/java/kr/co/ddamddam/project/service/ProjectService.java
+++ src/main/java/kr/co/ddamddam/project/service/ProjectService.java
@@ -28,6 +28,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -157,6 +158,7 @@ public class ProjectService {
             currProject.setOfferPeriod(dto.getOfferPeriod());
             currProject.setProjectIdx(dto.getBoardIdx());
             currProject.setProjectImg(uploadedFilePath);
+            currProject.setProjectDate(LocalDateTime.now());
 
             Project modifiedProject = projectRepository.save(currProject);
 
