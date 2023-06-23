dba49b2e83ebbeb1c8829883cb2e329184813d56
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-06-23 16:06:05 +0900

    fix project-paging 수정전

--- frontend/src/component/projects/mainpage/ProjectsMain.js
+++ frontend/src/component/projects/mainpage/ProjectsMain.js
@@ -5,41 +5,42 @@ import {PROJECT} from "../../common/config/HostConfig";
 import {Link, useNavigate} from "react-router-dom";
 import less from "../../../src_assets/less.png";
 import than from "../../../src_assets/than.png";
+import {Modal, Button} from 'react-bootstrap';
+import {getToken, getUserPosition} from "../../common/util/login-util";
+import 'bootstrap/dist/css/bootstrap.css';
+import '../scss/QuickMatching.scss';
 
-const ProjectsMain = ({headerInfo}) => {
+const ProjectsMain = () => {
 
-    //인기프로젝트 페이지 값
-    const [mainCurrentPage, setMainCurrentPage] = useState(1);
-    const handleCarouselIndexChange= (currentPage)=>{
-        setMainCurrentPage(currentPage);
-        console.log(`현재 main의 페이지 번호 : `,currentPage)
-    }
-  console.log(headerInfo)
-
+  // 토큰
+  const ACCESS_TOKEN = getToken();
+  const USER_POSITION = getUserPosition();
+  const headerInfo = {
+    'content-type': 'application/json',
+    'Authorization': 'Bearer ' + ACCESS_TOKEN
+  }
 
   const navigate = useNavigate();
   const childRef = useRef(null);
 
-  const pageSize = 3;
+  // url
+  const [currentUrl, setCurrentUrl] = useState(PROJECT);
 
-  const [currentUrl, setCurrentUrl]
-    = useState(PROJECT);
-  const popularity = PROJECT + `?sort=like&page=${mainCurrentPage}&size=${pageSize}`;
-  // const popularity = PROJECT + `?sort=like&page=1&size=3`;
-    useEffect(()=>{
+  // 인기프로젝트 페이지
+  const [likePage, setLikePage] = useState(1);
+  const LIKE_PAGE_SIZE = 3;
 
-    },[mainCurrentPage,popularity])
+  const handleCarouselIndexChange = (currentPage) => {
+    setLikePage(currentPage);
+    console.log(`현재 main의 페이지 번호 : `, currentPage)
+  }
+  const popularity = PROJECT + `?sort=like&page=${likePage}&size=${LIKE_PAGE_SIZE}`;
 
+  useEffect(() => {
+  }, [likePage, popularity])
 
-  const handleFrontClick = () => {
-    console.log("프론트")
-    setCurrentUrl(PROJECT + '?position=front');
-  };
-
-  const handleBackClick = () => {
-    console.log("백")
-    setCurrentUrl(PROJECT + '?position=back');
-  };
+  // 퀵 매칭 모달창
+  const [quickDetail, setQuickDetail] = useState([]);
 
   const handleLikeClick = (projectId) => {
     // 서버에 좋아요 처리를 위한 POST 요청을 보냅니다
@@ -60,14 +61,43 @@ const ProjectsMain = ({headerInfo}) => {
   };
 
 
-
   const handleShowDetails = (projectIdx) => {
     console.log('게시판 번호: ', projectIdx);
 
     navigate(`/projects/detail?projectIdx=${projectIdx}`, {state: {headerInfo}});
   };
 
+  // 퀵 매칭
+  const [show, setShow] = useState(false);
 
+  const handleShow = () => {
+    console.log('퀵 매칭 버튼 클릭');
+    setShow(true);
+    quickMatchingFetchData();
+  };
+
+  const handleClose = () => {
+    setShow(false)
+  };
+
+  const quick = PROJECT + `/quick?position=${USER_POSITION}&size=5`;
+  const quickMatchingFetchData = () => {
+    fetch(quick, {
+      method: 'GET',
+      headers: headerInfo
+    }).then(res => {
+        if (res.status === 500) {
+          alert('서버 오류입니다');
+          return;
+        }
+        return res.json();
+      }
+    ).then(res => {
+      console.log('퀵 매칭 데이터 패치');
+      console.log(res.payload.projects);
+      setQuickDetail(res.payload.projects);
+    })
+  };
 
   return (
     <>
@@ -80,8 +110,8 @@ const ProjectsMain = ({headerInfo}) => {
         sortTitle={'인기 프로젝트'}
         handleLikeClick={handleLikeClick}
         handleShowDetails={handleShowDetails}
-        onCarouselIndexChange = {handleCarouselIndexChange}
-        pageSize = {pageSize}
+        onCarouselIndexChange={handleCarouselIndexChange}
+        pageSize={LIKE_PAGE_SIZE}
         ref={childRef}
       />
 
@@ -92,6 +122,60 @@ const ProjectsMain = ({headerInfo}) => {
       {/*  handleShowDetails={handleShowDetails}*/}
       {/*  ref={childRef}*/}
       {/*/>*/}
+
+      {/* 퀵매칭 */}
+      <div className={'quick-wrapper'}>
+        <Button className={'quick-btn'}
+                onClick={handleShow}
+        >퀵 매칭
+        </Button>
+
+        <Modal show={show} onHide={handleClose} id="modal-container">
+
+          {quickDetail.map((q) => (
+            <>
+              <Modal.Header closeButton>
+                <Modal.Title>{q.boardTitle}</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div>나의 포지션 : {USER_POSITION}</div>
+                <div>제목 : {q.boardContent}</div>
+                <div>
+                  {Array(q.maxFront - q.applicantOfFront)
+                    .fill()
+                    .map((_, index) => (
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/128/4552/4552960.png"
+                        alt="front"
+                        style={{height: 60}}
+                        key={index}
+                      />
+                    ))}
+                  {Array(q.maxBack - q.applicantOfBack)
+                    .fill()
+                    .map((_, index) => (
+                      <img
+                        src="https://cdn-icons-png.flaticon.com/128/4552/4552941.png"
+                        alt="front"
+                        style={{height: 60}}
+                        key={index}
+                      />
+                    ))}
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  닫기
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                  저장
+                </Button>
+              </Modal.Footer>
+            </>
+          ))}
+
+        </Modal>
+      </div>
     </>
   );
 };
