a606f1ccf8c76aa0fbf3cbbf6e63c6b8dcb0a465
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-06-23 19:47:14 +0900

    Feat: api 24시간 갱신

--- frontend/src/component/user/UserFindPassword.js
+++ frontend/src/component/user/UserFindPassword.js
@@ -4,6 +4,9 @@ import axios from "axios";
 import {BASE_URL, AUTH} from "../common/config/HostConfig";
 import '../user/scss/UserFindPassword.scss';
 import {getToken} from "../common/util/login-util";
+import Common from "../common/Common";
+import logo from "../../src_assets/logo(white).png";
+import {BsCheckLg} from "react-icons/bs";
 
 const UserFindPassword = () => {
 
@@ -51,48 +54,69 @@ const UserFindPassword = () => {
 
 
   return (
-    <div className={'form-container'}>
-      <form className={'find-form'} onSubmit={handleSubmit}>
-        <h1 className={'password-title'}>👻비밀번호 변경👽</h1>
-        <br/>
-        ** 분실하지 않도록 신중하게 설정하세요 ! **
-        <br/><br/><br/>
-        <label className={'id'}>
-          사용자 아이디(이메일) :
-          <input
-            type="text"
-            name="userIdx"
-            className={'email-input'}
-            value={userIdx}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br/><br/>
-        {/*<label className={'oldpw'}>*/}
-        {/*  기존 비밀번호:*/}
-        {/*  <input*/}
-        {/*    type="password"*/}
-        {/*    name="newPassword"*/}
-        {/*    className={'oldpassword-input'}*/}
-        {/*    value={newPassword}*/}
-        {/*    onChange={handleInputChange}*/}
-        {/*  />*/}
-        {/*</label>*/}
-        {/*<br/><br/>*/}
-        <label className={'newpw'}>
-          새로운 비밀번호 :
-          <input
-            type="password"
-            name="newPassword"
-            className={'newpassword-input'}
-            value={newPassword}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br/><br/>
-        <button className={'changebtn'} type="submit">변경하기</button>
-      </form>
-    </div>
+    <Common className={'change-wrapper'}>
+      <section className={'top-wrapper'}>
+        <img src={logo} alt={'logo'} className={'logo'}/>
+        <div className={'main-title'}>HI,WE ARE<br/>DDAMDDAM CLUB</div>
+      </section>
+      <div className={'background'}></div>
+      <div className={'form-container'}>
+        <section className={'form-wrapper'} onSubmit={handleSubmit}>
+          <br/>
+          ** 분실하지 않도록 신중하게 설정하세요 ! **
+          <br/><br/><br/>
+          <div className={'input-nickname'}>
+            <label className={'id'}>
+              사용자 아이디(이메일) :
+              <input
+                type="text"
+                name="nickname"
+                className={'nickname'}
+                value={userIdx}
+                onChange={handleInputChange}
+              />
+              <span className={correct.userNickName ? 'correct' : 'not-correct'}>{message.userNickName}</span>
+            </label>
+          </div>
+          <br/><br/>
+          <label className={'oldpw'}>
+            기존 비밀번호 :
+            <input
+              type="password"
+              name="newPassword"
+              className={'oldpassword-input'}
+              value={newPassword}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br/><br/>
+          <label className={'newpw'}>
+            새로운 비밀번호 :
+            <input
+              type="password"
+              name="newPassword"
+              className={'newpassword-input'}
+              value={newPassword}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br/><br/>
+          <label className={'newpw-check'}>
+            비밀번호 확인 :
+            <input
+              type="password"
+              name="newPassword"
+              className={'passwordcheck-input'}
+              value={newPassword}
+              onChange={handleInputChange}
+            />
+          </label>
+          <br/><br/>
+          <button className={'changebtn'} type="submit">변경하기</button>
+        </section>
+      </div>
+    </Common>
+
   );
 };
 
