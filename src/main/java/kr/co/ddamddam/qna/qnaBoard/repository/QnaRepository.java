c7550986c4990683d16ec3be67c11e53d10d7369
JOY0987 <jjoyewon@gmail.com>
2023-06-24 15:59:32 +0900

    Fix: QNA 게시글 검색 수정 및 API 테스트 완료
    - 검색, 정렬 메서드 1개로 통일

--- src/main/java/kr/co/ddamddam/qna/qnaBoard/repository/QnaRepository.java
+++ src/main/java/kr/co/ddamddam/qna/qnaBoard/repository/QnaRepository.java
@@ -22,10 +22,10 @@ public interface QnaRepository extends JpaRepository<Qna, Long> {
 
     @Query("SELECT DISTINCT q FROM Qna q " +
             "JOIN q.hashtagList h " +
-            "WHERE (q.qnaTitle LIKE %:keyword% " +
+            "WHERE ((:keyword IS NULL OR :keyword = '') OR (q.qnaTitle LIKE %:keyword% " +
             "OR q.qnaContent LIKE %:keyword% " +
-            "OR h.hashtagContent LIKE %:keyword%) " +
-            "AND (:sort = 'ALL' OR q.qnaAdoption = :sort)" +
+            "OR h.hashtagContent LIKE %:keyword%)) " +
+            "AND ((:sort IS NULL OR :sort = '') OR q.qnaAdoption = :sort)" +
             "ORDER BY q.qnaDate DESC")
     Page<Qna> findByKeywordAndSort(
             @Param("keyword") String keyword,
