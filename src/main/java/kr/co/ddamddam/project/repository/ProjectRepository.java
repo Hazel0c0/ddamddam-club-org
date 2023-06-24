fac9ef918e71fe821a324b7abb021af8b44fe295
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-06-24 16:29:42 +0900

    ERROR quick matching select

--- src/main/java/kr/co/ddamddam/project/repository/ProjectRepository.java
+++ src/main/java/kr/co/ddamddam/project/repository/ProjectRepository.java
@@ -15,12 +15,12 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("SELECT p FROM Project p " +
         "WHERE (p.maxBack - SIZE(p.applicantOfBacks)) > 0 " +
-        "ORDER BY (p.maxBack - SIZE(p.applicantOfBacks)) ,p.projectDate")
+        "ORDER BY (p.maxBack - SIZE(p.applicantOfBacks)) ASC, p.projectDate ASC")
     Page<Project> backQuickSort(Pageable pageable);
 
     @Query("SELECT p FROM Project p " +
         "WHERE (p.maxFront - SIZE(p.applicantOfFronts)) > 0 " +
-        "ORDER BY (p.maxFront - SIZE(p.applicantOfFronts)) ,p.projectDate")
+        "ORDER BY (p.maxFront - SIZE(p.applicantOfFronts)) ASC, p.projectDate ASC")
     Page<Project> frontQuickSort(Pageable pageable);
 
     @Query("SELECT p FROM Project p WHERE LOWER(p.projectTitle) LIKE %:keyword%")
