e181c8990abc038197af803db6198e0922a59f5e
예진초이 <115179558+Yajani@users.noreply.github.com>
2023-06-25 17:35:57 +0900

    Feat: company 키워드 검색 수 & 프로필사진 변경하기 진행중

--- src/main/java/kr/co/ddamddam/company/repository/CompanyRepository.java
+++ src/main/java/kr/co/ddamddam/company/repository/CompanyRepository.java
@@ -7,15 +7,28 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+// TODO: 키워드 수정 필요 🍻
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
+
+//    @Query("SELECT DISTINCT c FROM Company c " +
+//            "WHERE (:keyword IS NULL OR :keyword = '' OR " +
+//            "c.companyTitle LIKE CONCAT('%', :keyword, '%') OR " +
+//            "c.companyCareer LIKE CONCAT('%', :keyword, '%') OR " +
+//            "c.companyArea LIKE CONCAT('%', :keyword, '%') OR " +
+//            "c.companyName LIKE CONCAT('%', :keyword, '%') OR " +
+//            "c.companySal LIKE CONCAT('%', :keyword, '%')) " +
+//            "AND (:sort IS NULL OR :sort = '' OR c.companyCareer = :sort) " +
+//            "AND (c.companyTitle LIKE '%백엔드%' OR " +
+//            "c.companyTitle LIKE '%back%' OR " +
+//            "c.companyTitle LIKE '%Back%')")
     //키워드 검색
     @Query("SELECT DISTINCT c FROM Company c " +
             "WHERE ((:keyword IS NULL OR : keyword = '') OR (c.companyTitle LIKE %:keyword% " +
             "OR c.companyCareer LIKE %:keyword% OR c.companyArea LIKE %:keyword% " +
             "OR c.companyName LIKE %:keyword% OR c.companySal LIKE %:keyword%)) " +
             "AND ((:sort IS NULL OR :sort = '') OR c.companyCareer = :sort)")
-    Page<Company> findByKeyword(@Param("keyword") String keyword,@Param("sort") String sort,Pageable pageable);
+    Page<Company> findByKeyword(@Param("keyword") String keyword, @Param("sort") String sort, Pageable pageable);
 
 
     @Query("SELECT c FROM Company c WHERE c.companyCareer ='경력' ")
