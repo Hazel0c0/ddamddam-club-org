cbd1d7469a8a7ae5bd51392c397afe80683907e0
oslob99 <jkh991116@naver.com>
2023-06-26 17:35:16 +0900

    Feat: 확정멘티 검증

--- frontend/src/component/mentors/MentorsChat.js
+++ frontend/src/component/mentors/MentorsChat.js
@@ -102,7 +102,13 @@ const MentorsChat = () => {
           method: 'PUT',
           headers: headerInfo
         })
-          .then((res) => res.json())
+          .then((res) => {
+            if(res.status === 400){
+              alert('이미 확정된 멘티입니다');
+              return;
+            } 
+            return res.json();
+          })
           .then((result) => {
             setMenteeCount(result);
           });
