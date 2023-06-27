0f40289a39f41141f9015de7a12e9f42cf6a4e14
JOY0987 <jjoyewon@gmail.com>
2023-06-27 19:49:32 +0900

    Fix: SecurityConfig 수정
    - 메인페이지 URL 추가

--- src/main/java/kr/co/ddamddam/config/SecurityConfig.java
+++ src/main/java/kr/co/ddamddam/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                 .authorizeRequests()
                     // 어떤 요청에서 인증을 안할 것인지 설정
                     // - 회원가입, 로그인, index 페이지, 각 게시판 첫 페이지
+                    .antMatchers("/").permitAll()
                     .antMatchers("/api/ddamddam/auth/load-profile").permitAll()
                     .antMatchers("/api/ddamddam/auth/**").permitAll()
                     .antMatchers("/api/ddamddam/email/**").permitAll()
