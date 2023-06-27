c847cf26791a60a8e8adda37345324dd3d4f8c6b
Hazel0c0 <120666603+Hazel0c0@users.noreply.github.com>
2023-06-27 18:37:11 +0900

    FIX pop page

--- frontend/src/component/projects/PopularProjects.js
+++ frontend/src/component/projects/PopularProjects.js
@@ -11,110 +11,111 @@ import {PROJECT} from "../common/config/HostConfig";
 import ProjectListContainer from "./ProjectListContainer";
 
 const PopularProjects = forwardRef((
-        {
-            sortTitle,
-            handleLikeClick,
-            handleShowDetails,
-            keyword
-        },
-        ref
-    ) => {
+    {
+      sortTitle,
+      handleLikeClick,
+      handleShowDetails,
+      keyword
+    },
+    ref
+  ) => {
 
 // 키워드 null값 처리
-        const navigate = useNavigate();
-        const [projects, setProjects] = useState([]);
+    const navigate = useNavigate();
+    const [projects, setProjects] = useState([]);
 
-        const [pageNation, setPageNation] = useState([]);
-        const [prevBtn, setPrevBtn] = useState(false);
-        const [nextBtn, setNextBtn] = useState(false);
-        //캐러셀
-        const [carouselIndex, setCarouselIndex] = useState(1);
-        const [clickCurrentPage, setClickCurrentPage] = useState(1);
+    const [pageNation, setPageNation] = useState([]);
+    const [prevBtn, setPrevBtn] = useState(false);
+    const [nextBtn, setNextBtn] = useState(false);
+    //캐러셀
+    const [carouselIndex, setCarouselIndex] = useState(1);
+    const [clickCurrentPage, setClickCurrentPage] = useState(1);
 
-        const LIKE_PAGE_SIZE = 3;
+    const LIKE_PAGE_SIZE = 3;
 
-        const currentPageHandler = (clickPageNum) => {
-            console.log(`페이지 클릭 시 현재 페이지 번호: ${clickPageNum}`);
-            setClickCurrentPage(clickPageNum);
-        }
-        useImperativeHandle(ref, () => ({
-            fetchData
-        }));
-
-        let FETCH;
-        if (!!keyword) {
-             FETCH = `${PROJECT}?page=${carouselIndex}&size=${LIKE_PAGE_SIZE}&like=true&keyword=${keyword}`;
-        } else {
-             FETCH = `${PROJECT}?page=${carouselIndex}&size=${LIKE_PAGE_SIZE}&like=true`;
-        }
-        const fetchData = () => {
-            fetch(FETCH, {
-                method: 'GET',
-                headers: {'content-type': 'application/json'}
-            })
-                .then(res => {
-                    if (res.status === 500) {
-                        alert('잠시 후 다시 접속해주세요.[서버오류]');
-                        return;
-                    }
-                    return res.json();
-                })
-                .then(result => {
-                    if (!!result) {
-                        // console.log(`result의 값 : `, result);
-                        console.log(`projects : `, result.projects);
-                        console.log(`pageInfo : `, result.pageInfo);
-                        setProjects(result.projects);
-                        setPageNation(result.pageInfo);
-                        console.log(`carouselIndex : ${carouselIndex}`)
-                    }
-                });
-        };
-
-        useEffect(() => {
-            fetchData();
-        }, [carouselIndex, keyword]);
-
-        // 페이지 처리
-        const handlePrevious = () => {
-            if (pageNation.currentPage > 1) {
-                console.log(`pageNation.currentPage : ${pageNation.currentPage}`)
-                setCarouselIndex(prevIndex => prevIndex - 1);
-            }
-        };
-
-
-        const handleNext = () => {
-            console.log('handleNext')
-
-            if (pageNation.next === true) {
-                setCarouselIndex(prevIndex => prevIndex + 1);
-                console.log(`pageNation.currentPage : ${pageNation.currentPage}`)
-            }
-        };
-
-
-        return (
-            <Common className={'project-list-wrapper'}>
-                {pageNation.currentPage > 1 &&
-                    <img src={less} alt={"less-icon"} className={'less-icon'} onClick={handlePrevious}/>
-                }
-
-                {pageNation.next &&
-                    <img src={than} alt={"than-icon"} className={'than-icon'} onClick={handleNext}/>
-                }
-
-                <h2 className={'sort-title'}>{sortTitle}</h2>
-
-                <ProjectListContainer
-                    projects={projects}
-                    handleShowDetails={handleShowDetails}
-                    handleLikeClick={handleLikeClick}
-                />
-
-            </Common>
-        );
+    const currentPageHandler = (clickPageNum) => {
+      console.log(`페이지 클릭 시 현재 페이지 번호: ${clickPageNum}`);
+      setClickCurrentPage(clickPageNum);
     }
+    useImperativeHandle(ref, () => ({
+      fetchData
+    }));
+
+    let FETCH;
+    if (!!keyword) {
+      FETCH = `${PROJECT}?page=${carouselIndex}&size=${LIKE_PAGE_SIZE}&like=true&keyword=${keyword}`;
+    } else {
+      FETCH = `${PROJECT}?page=${carouselIndex}&size=${LIKE_PAGE_SIZE}&like=true`;
+    }
+    const fetchData = () => {
+      fetch(FETCH, {
+        method: 'GET',
+        headers: {'content-type': 'application/json'}
+      })
+        .then(res => {
+          if (res.status === 500) {
+            alert('잠시 후 다시 접속해주세요.[서버오류]');
+            return;
+          }
+          return res.json();
+        })
+        .then(result => {
+          if (!!result) {
+            // console.log(`result의 값 : `, result);
+            console.log(`projects : `, result.projects);
+            console.log(`pageInfo : `, result.pageInfo);
+            setProjects(result.projects);
+            setPageNation(result.pageInfo);
+            console.log(`carouselIndex : ${carouselIndex}`)
+          }
+        });
+    };
+
+    useEffect(() => {
+      fetchData();
+    }, [carouselIndex, keyword]);
+
+    // 페이지 처리
+    const handlePrevious = () => {
+      if (pageNation.currentPage > 1) {
+        console.log(`pageNation.currentPage : ${pageNation.currentPage}`)
+        setCarouselIndex(prevIndex => prevIndex - 1);
+      }
+    };
+
+    const lastPage = Math.ceil(pageNation.totalCount / 3);
+
+    const handleNext = () => {
+      console.log('handleNext')
+
+      if (pageNation.currentPage < lastPage) {
+        setCarouselIndex(prevIndex => prevIndex + 1);
+        console.log(`pageNation.currentPage : ${pageNation.currentPage}`)
+      }
+    };
+
+
+    return (
+      <Common className={'project-list-wrapper'}>
+        {pageNation.currentPage > 1 &&
+          <img src={less} alt={"less-icon"} className={'less-icon'} onClick={handlePrevious}/>
+        }
+
+        {pageNation.currentPage < lastPage &&
+          <img src={than} alt={"than-icon"} className={'than-icon'} onClick={handleNext}/>
+        }
+
+        <h2 className={'sort-title'}>{sortTitle}</h2>
+
+        <ProjectListContainer
+          projects={projects}
+          handleShowDetails={handleShowDetails}
+          handleLikeClick={handleLikeClick}
+        />
+
+      </Common>
+    );
+  }
 );
 
 export default PopularProjects;
--- src/main/java/kr/co/ddamddam/project/service/ProjectService.java
+++ src/main/java/kr/co/ddamddam/project/service/ProjectService.java
@@ -37,198 +37,196 @@ import static kr.co.ddamddam.common.exception.custom.ErrorCode.*;
 @RequiredArgsConstructor
 @Transactional
 public class ProjectService {
-    private final ProjectRepository projectRepository;
-    private final UserRepository userRepository;
-    private final ValidateToken validateToken;
+  private final ProjectRepository projectRepository;
+  private final UserRepository userRepository;
+  private final ValidateToken validateToken;
 
-    public ProjectListPageResponseDTO getList(ProjectPageDTO dto, ProjectSearchRequestDto searchDto) {
+  public ProjectListPageResponseDTO getList(ProjectPageDTO dto, ProjectSearchRequestDto searchDto) {
+    log.info("pageDTO : {} ", dto);
 
-        // 기본(최신순), 좋아요, 퀵 매칭 정렬
-        Pageable pageable = getPageable(dto, searchDto);
+    // 기본(최신순), 좋아요, 퀵 매칭 정렬
+    Pageable pageable = getPageable(dto, searchDto);
 
-        // 검색 - F, B, 키워드
-        Page<Project> projectPage = search(pageable, searchDto);
+    // 검색 - F, B, 키워드
+    Page<Project> projectPage = search(pageable, searchDto);
 
-        return listPaging(projectPage);
+    return listPaging(projectPage);
+  }
+
+  private Pageable getPageable(ProjectPageDTO dto, ProjectSearchRequestDto searchDto) {
+
+    if (searchDto.isLike()) {
+      log.info("is like pageable !!");
+      return PageRequest.of(
+          dto.getPage() - 1,
+          dto.getSize(),
+          Sort.by("likeCount").descending()
+      );
+      // 퀵 매칭 (포지션) 정렬
+    } else if (StringUtils.isNotEmpty(searchDto.getPosition())) {
+      log.info("position pageable !!");
+      return PageRequest.of(
+          dto.getPage() - 1,
+          dto.getSize()
+      );
+    }
+    // 최신순 정렬 : 기본값
+    log.info("new pageable !!");
+    return PageRequest.of(
+        dto.getPage() - 1,
+        dto.getSize(),
+        Sort.by("projectDate").descending()
+    );
+
+  }
+
+  private static ProjectListPageResponseDTO listPaging(Page<Project> projectPage) {
+    List<Project> projects = projectPage.getContent();
+    List<ProjectDetailResponseDTO> projectList = projects.stream()
+        .map(project -> new ProjectDetailResponseDTO(project))
+        .collect(Collectors.toList());
+
+    return ProjectListPageResponseDTO.builder()
+        .count(projectList.size())
+        .pageInfo(new ProjectPageResponseDTO<Project>(projectPage))
+        .projects(projectList)
+        .build();
+  }
+
+
+  private Page<Project> search(Pageable pageable, ProjectSearchRequestDto searchDto) {
+
+    // 포지션별 조회 : 포지션별 남은자리 적은 순 정렬
+    if ("FRONTEND".equals(searchDto.getPosition())) {
+      log.info("position FRONTEND search !!");
+      return projectRepository.frontQuickSort(pageable);
+
+    } else if ("BACKEND".equals(searchDto.getPosition())) {
+      log.info("position BACKEND search !!");
+      return projectRepository.backQuickSort(pageable);
     }
 
-    private Pageable getPageable(ProjectPageDTO dto, ProjectSearchRequestDto searchDto) {
-        Pageable pageable = null;
-
-        if (searchDto.isLike()) {
-            log.info("is like pageable !!");
-            pageable = PageRequest.of(
-                    dto.getPage() - 1,
-                    dto.getSize(),
-                    Sort.by("likeCount").descending()
-            );
-            // 퀵 매칭 (포지션) 정렬
-        } else if (StringUtils.isNotEmpty(searchDto.getPosition())) {
-            log.info("position pageable !!");
-            pageable = PageRequest.of(
-                    dto.getPage() - 1,
-                    dto.getSize()
-            );
-        } else {
-            // 최신순 정렬 : 기본값
-            log.info("new pageable !!");
-            pageable = PageRequest.of(
-                    dto.getPage() - 1,
-                    dto.getSize(),
-                    Sort.by("projectDate").descending()
-            );
-        }
-
-        return pageable;
+    // 검색어 조회
+    if (searchDto.getKeyword() != null) {
+      log.info("KEYWORD search !! : {}", searchDto.getKeyword());
+      return projectRepository.findProjectsBySearchWord(pageable, searchDto.getKeyword());
     }
+    // 조건 X
+    log.info("search !!");
+    return projectRepository.findAll(pageable);
+  }
 
-    private static ProjectListPageResponseDTO listPaging(Page<Project> projectPage) {
-        List<Project> projects = projectPage.getContent();
-        List<ProjectDetailResponseDTO> projectList = projects.stream()
-                .map(project -> new ProjectDetailResponseDTO(project))
-                .collect(Collectors.toList());
+  public ProjectDetailResponseDTO getDetail(Long projectIdx, TokenUserInfo tokenUserInfo) {
 
-        return ProjectListPageResponseDTO.builder()
-                .count(projectList.size())
-                .pageInfo(new ProjectPageResponseDTO<Project>(projectPage))
-                .projects(projectList)
-                .build();
-    }
+    validateToken.validateToken(tokenUserInfo);
+
+    Project foundProject = getProject(projectIdx);
+
+    return new ProjectDetailResponseDTO(foundProject);
+  }
+
+  public Project getProject(Long projectIdx) {
+    return projectRepository.findById(projectIdx)
+        .orElseThrow(() -> new RuntimeException(projectIdx + "번 게시물이 존재하지 않습니다!"));
+  }
+
+  // 글 작성
+  public ProjectDetailResponseDTO write(
+      final TokenUserInfo tokenUserInfo,
+      final ProjectWriteDTO dto,
+      final String uploadedFilePath
+  ) {
+    validateToken.validateToken(tokenUserInfo);
+
+    Long userIdx = Long.valueOf(tokenUserInfo.getUserIdx());
+
+    User user = userRepository.findById(userIdx)
+        .orElseThrow(() -> new RuntimeException(userIdx + "회원이 존재하지 않습니다!"));
+
+    Project saved = projectRepository.save(dto.toEntity(user, uploadedFilePath));
+
+    return new ProjectDetailResponseDTO(saved);
+  }
 
 
-    private Page<Project> search(Pageable pageable, ProjectSearchRequestDto searchDto) {
+  public ProjectDetailResponseDTO modify(
+      TokenUserInfo tokenUserInfo,
+      ProjectModifyRequestDTO dto,
+      String uploadedFilePath
+  ) {
+    validateDTO(tokenUserInfo, dto.getBoardIdx());
 
-        // 포지션별 조회 : 포지션별 남은자리 적은 순 정렬
-        if ("FRONTEND".equals(searchDto.getPosition())) {
-            log.info("position FRONTEND search !!");
-            return projectRepository.frontQuickSort(pageable);
+    Project currProject = getProject(dto.getBoardIdx());
 
-        } else if ("BACKEND".equals(searchDto.getPosition())) {
-            log.info("position BACKEND search !!");
-            return projectRepository.backQuickSort(pageable);
-        }
-
-        // 검색어 조회
-        if (searchDto.getKeyword() != null) {
-            log.info("KEYWORD search !! : {}", searchDto.getKeyword());
-            return projectRepository.findProjectsBySearchWord(pageable, searchDto.getKeyword());
-        }
-        // 조건 X
-        log.info("search !!");
-        return projectRepository.findAll(pageable);
-    }
-
-    public ProjectDetailResponseDTO getDetail(Long projectIdx, TokenUserInfo tokenUserInfo) {
-
-        validateToken.validateToken(tokenUserInfo);
-
-        Project foundProject = getProject(projectIdx);
-
-        return new ProjectDetailResponseDTO(foundProject);
-    }
-
-    public Project getProject(Long projectIdx) {
-        return projectRepository.findById(projectIdx)
-                .orElseThrow(() -> new RuntimeException(projectIdx + "번 게시물이 존재하지 않습니다!"));
-    }
-
-    // 글 작성
-    public ProjectDetailResponseDTO write(
-            final TokenUserInfo tokenUserInfo,
-            final ProjectWriteDTO dto,
-            final String uploadedFilePath
+    if (currProject.getUser().getUserEmail().equals(
+        tokenUserInfo.getUserEmail())
     ) {
-        validateToken.validateToken(tokenUserInfo);
+      log.info("이 게시글 작성자와 현재 로그인 사용자가 일치합니다");
 
-        Long userIdx = Long.valueOf(tokenUserInfo.getUserIdx());
+      currProject.setProjectTitle(dto.getBoardTitle());
+      currProject.setProjectContent(dto.getBoardContent());
+      currProject.setProjectType(dto.getProjectType());
+      currProject.setMaxFront(dto.getMaxFront());
+      currProject.setMaxBack(dto.getMaxBack());
+      currProject.setOfferPeriod(dto.getOfferPeriod());
+      currProject.setProjectIdx(dto.getBoardIdx());
+      currProject.setProjectImg(uploadedFilePath);
+      currProject.setProjectDate(LocalDateTime.now());
 
-        User user = userRepository.findById(userIdx)
-                .orElseThrow(() -> new RuntimeException(userIdx + "회원이 존재하지 않습니다!"));
+      Project modifiedProject = projectRepository.save(currProject);
 
-        Project saved = projectRepository.save(dto.toEntity(user, uploadedFilePath));
+      return new ProjectDetailResponseDTO(modifiedProject);
+    } else {
+      throw new UnauthorizationException(ErrorCode.ACCESS_FORBIDDEN, tokenUserInfo.getUserEmail());
+    }
+  }
 
-        return new ProjectDetailResponseDTO(saved);
+  public void delete(Long id) {
+    projectRepository.deleteById(id);
+  }
+
+  // 퀵 매칭
+  // select : 오래된 순 / 내 포지션 일치 / 남은자리가 적은것 부터
+  public ProjectListPageResponseDTO quickMatching(TokenUserInfo tokenUserInfo, ProjectPageDTO dto, ProjectSearchRequestDto searchDto) {
+    validateToken.validateToken(tokenUserInfo);
+
+    Pageable pageable = getPageable(dto, searchDto);
+
+    Page<Project> projectPage = null;
+    try {
+      projectPage = search(pageable, searchDto);
+      log.info("ProjectService/quickMatching/projectPage {}", projectPage);
+    } catch (Exception e) {
+      log.error("quickMatching error !! : {}", e.getMessage());
     }
 
+    return listPaging(projectPage);
+  }
 
-    public ProjectDetailResponseDTO modify(
-            TokenUserInfo tokenUserInfo,
-            ProjectModifyRequestDTO dto,
-            String uploadedFilePath
-    ) {
-        validateDTO(tokenUserInfo, dto.getBoardIdx());
-
-        Project currProject = getProject(dto.getBoardIdx());
-
-        if (currProject.getUser().getUserEmail().equals(
-                tokenUserInfo.getUserEmail())
-        ) {
-            log.info("이 게시글 작성자와 현재 로그인 사용자가 일치합니다");
-
-            currProject.setProjectTitle(dto.getBoardTitle());
-            currProject.setProjectContent(dto.getBoardContent());
-            currProject.setProjectType(dto.getProjectType());
-            currProject.setMaxFront(dto.getMaxFront());
-            currProject.setMaxBack(dto.getMaxBack());
-            currProject.setOfferPeriod(dto.getOfferPeriod());
-            currProject.setProjectIdx(dto.getBoardIdx());
-            currProject.setProjectImg(uploadedFilePath);
-            currProject.setProjectDate(LocalDateTime.now());
-
-            Project modifiedProject = projectRepository.save(currProject);
-
-            return new ProjectDetailResponseDTO(modifiedProject);
-        } else {
-            throw new UnauthorizationException(ErrorCode.ACCESS_FORBIDDEN, tokenUserInfo.getUserEmail());
-        }
+  /**
+   * 토큰 유효성을 검사합니다.
+   * 로그인 중인 유저와 게시글 작성자가 동일한지 검사합니다.
+   *
+   * @param tokenUserInfo
+   * @param boardIdx
+   * @return
+   */
+  private Project validateDTO(TokenUserInfo tokenUserInfo, Long boardIdx) {
+    // 토큰 인증 실패
+    if (tokenUserInfo == null) {
+      throw new UnauthorizationException(UNAUTHENTICATED_USER, "로그인 후 이용 가능합니다.");
     }
 
-    public void delete(Long id) {
-        projectRepository.deleteById(id);
+    // 게시글 존재 여부 확인
+    Project project = projectRepository.findById(boardIdx).orElseThrow(() -> {
+      throw new NotFoundBoardException(NOT_FOUND_BOARD, boardIdx);
+    });
+
+    // 토큰 내 회원 이메일과 게시글 작성자의 이메일이 일치하지 않음 -> 작성자가 아님 -> 수정 및 삭제 불가
+    if (!project.getUser().getUserEmail().equals(tokenUserInfo.getUserEmail())) {
+      throw new UnauthorizationException(ACCESS_FORBIDDEN, tokenUserInfo.getUserEmail());
     }
 
-    // 퀵 매칭
-    // select : 오래된 순 / 내 포지션 일치 / 남은자리가 적은것 부터
-    public ProjectListPageResponseDTO quickMatching(TokenUserInfo tokenUserInfo, ProjectPageDTO dto, ProjectSearchRequestDto searchDto) {
-        validateToken.validateToken(tokenUserInfo);
-
-        Pageable pageable = getPageable(dto, searchDto);
-
-        Page<Project> projectPage = null;
-        try {
-            projectPage = search(pageable, searchDto);
-            log.info("ProjectService/quickMatching/projectPage {}", projectPage);
-        } catch (Exception e) {
-            log.error("quickMatching error !! : {}", e.getMessage());
-        }
-
-        return listPaging(projectPage);
-    }
-
-    /**
-     * 토큰 유효성을 검사합니다.
-     * 로그인 중인 유저와 게시글 작성자가 동일한지 검사합니다.
-     *
-     * @param tokenUserInfo
-     * @param boardIdx
-     * @return
-     */
-    private Project validateDTO(TokenUserInfo tokenUserInfo, Long boardIdx) {
-        // 토큰 인증 실패
-        if (tokenUserInfo == null) {
-            throw new UnauthorizationException(UNAUTHENTICATED_USER, "로그인 후 이용 가능합니다.");
-        }
-
-        // 게시글 존재 여부 확인
-        Project project = projectRepository.findById(boardIdx).orElseThrow(() -> {
-            throw new NotFoundBoardException(NOT_FOUND_BOARD, boardIdx);
-        });
-
-        // 토큰 내 회원 이메일과 게시글 작성자의 이메일이 일치하지 않음 -> 작성자가 아님 -> 수정 및 삭제 불가
-        if (!project.getUser().getUserEmail().equals(tokenUserInfo.getUserEmail())) {
-            throw new UnauthorizationException(ACCESS_FORBIDDEN, tokenUserInfo.getUserEmail());
-        }
-
-        return project;
-    }
+    return project;
+  }
 }
