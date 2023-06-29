83e9d86b446e326a7f7e877e2f598cd369101e83
JOY0987 <jjoyewon@gmail.com>
2023-06-29 15:52:47 +0900

    Fix: 공통 에러 핸들러 리턴타입 수정
    - 모든 에러가 상태코드 200으로 뜨는 문제 수정 (ResponseEntity 리턴타입으로 수정)

--- src/main/java/kr/co/ddamddam/common/exception/handler/CommonExceptionHandler.java
+++ src/main/java/kr/co/ddamddam/common/exception/handler/CommonExceptionHandler.java
@@ -6,6 +6,7 @@ import kr.co.ddamddam.common.exception.custom.NotFoundBoardException;
 import kr.co.ddamddam.common.exception.custom.NotFoundReplyException;
 import kr.co.ddamddam.common.exception.custom.NotFoundUserException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,29 +19,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CommonExceptionHandler {
 
     @ExceptionHandler(NotFoundBoardException.class)
-    public ExceptionResponse<NotFoundBoardException, Long> boardException(NotFoundBoardException e) {
+    public ResponseEntity<?> boardException(NotFoundBoardException e) {
         // 에러 코드, 에러 메세지, 에러가 발생한 QNA 게시글 번호
         log.error("notFoundQnaBoardException : {} {} {}", e.getErrorCode(), e.getMessage(), e.getIdx());
-        return new ExceptionResponse<>(e, e.getIdx());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus().value()).body(e.getMessage());
     }
 
     @ExceptionHandler(NotFoundReplyException.class)
-    public ExceptionResponse<NotFoundReplyException, Long> replyException(NotFoundReplyException e) {
+    public ResponseEntity<?> replyException(NotFoundReplyException e) {
         // 에러 코드, 에러 메세지, 에러가 발생한 QNA 게시글 번호
         log.error("notFoundQnaBoardException : {} {} {}", e.getErrorCode(), e.getMessage(), e.getIdx());
-        return new ExceptionResponse<>(e, e.getIdx());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus().value()).body(e.getMessage());
     }
 
     @ExceptionHandler(NotFoundUserException.class)
-    public ExceptionResponse<NotFoundUserException, Long> userException(NotFoundUserException e) {
+    public ResponseEntity<?> userException(NotFoundUserException e) {
         log.error("notFoundUserException : {} {} {}", e.getErrorCode(), e.getMessage(), e.getUserIdx());
-        return new ExceptionResponse<>(e, e.getUserIdx());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus().value()).body(e.getMessage());
     }
 
     @ExceptionHandler(UnauthorizationException.class)
-    public ExceptionResponse<UnauthorizationException, String> userException(UnauthorizationException e) {
+    public ResponseEntity<?> userException(UnauthorizationException e) {
         log.error("notFoundUserException : {} {} {}", e.getErrorCode(), e.getMessage(), e.getStr());
-        return new ExceptionResponse<>(e, e.getStr());
+        return ResponseEntity.status(e.getErrorCode().getHttpStatus().value()).body(e.getMessage());
     }
 
 }
